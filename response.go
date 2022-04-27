72b732b9154aac44f92812810843596655f28739
Eugene Kartavchenko <ewg@gismeteo.ru>
2022-04-27 03:03:10 +0300

    1

--- response.go
+++ response.go
@@ -1,240 +1,266 @@
 package alice
 
 import (
-	"errors"
-	"fmt"
-	"math/rand"
-	"strings"
+    "errors"
+    "fmt"
+    "math/rand"
+    "strings"
 
-	"github.com/azzzak/alice/effects"
+    "github.com/azzzak/alice/effects"
 )
 
 // Response структура исходящего сообщения.
 type Response struct {
-	Response *struct {
-		Text       string   `json:"text"`
-		TTS        string   `json:"tts,omitempty"`
-		Card       *Card    `json:"card,omitempty"`
-		Buttons    []Button `json:"buttons,omitempty"`
-		EndSession bool     `json:"end_session"`
-	} `json:"response,omitempty"`
+    Response *struct {
+	Text       string   `json:"text"`
+	TTS        string   `json:"tts,omitempty"`
+	Card       *Card    `json:"card,omitempty"`
+	Buttons    []Button `json:"buttons,omitempty"`
+	EndSession bool     `json:"end_session"`
+    } `json:"response,omitempty"`
+    Text_       string   `json:"text"`
+    TTS_        string   `json:"tts,omitempty"`
+    Card_       *Card    `json:"card,omitempty"`
+    Buttons_    []Button `json:"buttons,omitempty"`
+    EndSession_ bool     `json:"end_session"`
 
-	StartAccountLinking *struct{} `json:"start_account_linking,omitempty"`
+    StartAccountLinking *struct{} `json:"start_account_linking,omitempty"`
 
-	Session struct {
-		MessageID int    `json:"message_id"`
-		SessionID string `json:"session_id"`
-		UserID    string `json:"user_id"`
-	} `json:"session"`
+    Session struct {
+	MessageID int    `json:"message_id"`
+	SessionID string `json:"session_id"`
+	UserID    string `json:"user_id"`
+    } `json:"session"`
 
-	SessionState interface{} `json:"session_state,omitempty"`
+    SessionState interface{} `json:"session_state,omitempty"`
 
-	Version string `json:"version"`
+    Version string `json:"version"`
 }
 
 func (resp *Response) clean() *Response {
-	resp.Response = &struct {
-		Text       string   `json:"text"`
-		TTS        string   `json:"tts,omitempty"`
-		Card       *Card    `json:"card,omitempty"`
-		Buttons    []Button `json:"buttons,omitempty"`
-		EndSession bool     `json:"end_session"`
-	}{}
-	resp.StartAccountLinking = nil
-	resp.SessionState = nil
-	return resp
+    resp.Response = &struct {
+	Text       string   `json:"text"`
+	TTS        string   `json:"tts,omitempty"`
+	Card       *Card    `json:"card,omitempty"`
+	Buttons    []Button `json:"buttons,omitempty"`
+	EndSession bool     `json:"end_session"`
+    }{}
+    resp.Text_ = ""
+    resp.TTS_ = ""
+    resp.Card_ = nil
+    resp.Buttons_ = nil
+    resp.EndSession_ = false
+    resp.StartAccountLinking = nil
+    resp.SessionState = nil
+    return resp
 }
 
 func (resp *Response) prepareResponse(req *Request) *Response {
-	resp.Session.MessageID = req.Session.MessageID
-	resp.Session.SessionID = req.Session.SessionID
-	resp.Session.UserID = req.Session.UserID
-	resp.Version = "1.0"
-	return resp
+    resp.Session.MessageID = req.Session.MessageID
+    resp.Session.SessionID = req.Session.SessionID
+    resp.Session.UserID = req.Session.UserID
+    resp.Version = "1.0"
+    return resp
 }
 
 // StartAuthorization начать создание связки аккаунтов и показать пользователю карточку авторизации.
 func (resp *Response) StartAuthorization() *Response {
-	// resp.Response = Response{}
-	resp.StartAccountLinking = &struct{}{}
-	resp.Response = nil
-	return resp
+    // resp.Response = Response{}
+    resp.StartAccountLinking = &struct{}{}
+    resp.Response = nil
+    return resp
 }
 
 // Text добавляет строку к текстовому ответу. Если передано несколько строк, они будут разделены пробелом.
 func (resp *Response) Text(s ...string) *Response {
-	resp.Response.Text += strings.Join(s, " ")
-	return resp
+    resp.Response.Text += strings.Join(s, " ")
+    resp.Text_ += strings.Join(s, " ")
+    return resp
 }
 
 // RandomText добавляет к текстовому ответу случайную строку из числа предложенных.
 func (resp *Response) RandomText(s ...string) *Response {
-	ix := rand.Intn(len(s))
-	resp.Response.Text += s[ix]
-	return resp
+    ix := rand.Intn(len(s))
+    resp.Response.Text += s[ix]
+    resp.Text_ += s[ix]
+    return resp
 }
 
 // Space добавляет пробел к текстовому ответу.
 func (resp *Response) Space() *Response {
-	resp.Response.Text += " "
-	return resp
+    resp.Response.Text += " "
+    resp.Text_ += " "
+    return resp
 }
 
 // S синоним метода Space().
 func (resp *Response) S() *Response {
-	return resp.Space()
+    return resp.Space()
 }
 
 // ResetText обнуляет текстовый ответ.
 func (resp *Response) ResetText() *Response {
-	resp.Response.Text = ""
-	return resp
+    resp.Response.Text = ""
+    resp.Text_ = ""
+    return resp
 }
 
 // TTS добавляет строку к TTS. Если передано несколько строк, они будут разделены пробелом.
 func (resp *Response) TTS(tts ...string) *Response {
-	resp.Response.TTS += strings.Join(tts, " ")
-	return resp
+    resp.Response.TTS += strings.Join(tts, " ")
+    resp.TTS_ += strings.Join(tts, " ")
+    return resp
 }
 
 // TextWithTTS добавляет строки к текстовому ответу и к TTS.
 func (resp *Response) TextWithTTS(s, tts string) *Response {
-	return resp.Text(s).TTS(tts)
+    return resp.Text(s).TTS(tts)
 }
 
 // Pause добавляет паузу к TTS (знак "-").
 func (resp *Response) Pause(n int) *Response {
-	if n < 1 {
-		return resp
-	}
-
-	p := strings.Repeat("- ", n)
-	resp.Response.TTS += " " + p
+    if n < 1 {
 	return resp
+    }
+
+    p := strings.Repeat("- ", n)
+    resp.Response.TTS += " " + p
+    resp.TTS_ += " " + p
+    return resp
 }
 
 // Effect накладывает звуковой эффект на TTS.
 func (resp *Response) Effect(effect string) *Response {
-	e := fmt.Sprintf("<speaker effect=\"%s\">", effect)
-	resp.Response.TTS += e
-	return resp
+    e := fmt.Sprintf("<speaker effect=\"%s\">", effect)
+    resp.Response.TTS += e
+    resp.TTS_ += e
+    return resp
 }
 
 // NoEffect отключает наложенный на TTS эффект.
 func (resp *Response) NoEffect() *Response {
-	return resp.Effect(effects.NoEffect)
+    return resp.Effect(effects.NoEffect)
 }
 
 // Sound добавляет к TTS звук из библиотеки Алисы.
 func (resp *Response) Sound(sound string) *Response {
-	sound = strings.TrimSuffix(sound, ".opus")
-	s := fmt.Sprintf("<speaker sound=\"%s.opus\">", sound)
-	resp.Response.TTS += s
-	return resp
+    sound = strings.TrimSuffix(sound, ".opus")
+    s := fmt.Sprintf("<speaker sound=\"%s.opus\">", sound)
+    resp.Response.TTS += s
+    resp.TTS_ += s
+    return resp
 }
 
 // CustomSound добавляет к TTS собственный звук.
 func (resp *Response) CustomSound(skill, sound string) *Response {
-	sound = strings.TrimSuffix(sound, ".opus")
-	s := fmt.Sprintf("<speaker audio='dialogs-upload/%s/%s.opus'>", skill, sound)
-	resp.Response.TTS += s
-	return resp
+    sound = strings.TrimSuffix(sound, ".opus")
+    s := fmt.Sprintf("<speaker audio='dialogs-upload/%s/%s.opus'>", skill, sound)
+    resp.Response.TTS += s
+    resp.TTS_ += s
+    return resp
 }
 
 // ResetTTS обнуляет TTS.
 func (resp *Response) ResetTTS() *Response {
-	resp.Response.TTS = ""
-	return resp
+    resp.Response.TTS = ""
+    resp.TTS_ = ""
+    return resp
 }
 
 // Button структура кнопки.
 type Button struct {
-	Title   string      `json:"title"`
-	URL     string      `json:"url,omitempty"`
-	Payload interface{} `json:"payload,omitempty"`
-	Hide    bool        `json:"hide,omitempty"`
+    Title   string      `json:"title"`
+    URL     string      `json:"url,omitempty"`
+    Payload interface{} `json:"payload,omitempty"`
+    Hide    bool        `json:"hide,omitempty"`
 }
 
 // NewButton создает кнопку. Payload может быть проигнорирован. Если задано больше одного payload используется только первый.
 func NewButton(title, url string, hide bool, payload ...interface{}) Button {
-	var p interface{}
-	if len(payload) > 0 {
-		p = payload[0]
-	}
+    var p interface{}
+    if len(payload) > 0 {
+	p = payload[0]
+    }
 
-	return Button{
-		Title:   title,
-		URL:     url,
-		Hide:    hide,
-		Payload: p,
-	}
+    return Button{
+	Title:   title,
+	URL:     url,
+	Hide:    hide,
+	Payload: p,
+    }
 }
 
 // Button создает кнопку и добавляет ее в ответ. Payload может быть проигнорирован. Если задано больше одного payload используется только первый.
 func (resp *Response) Button(title, url string, hide bool, payload ...interface{}) *Response {
-	var p interface{}
-	if len(payload) > 0 {
-		p = payload[0]
-	}
+    var p interface{}
+    if len(payload) > 0 {
+	p = payload[0]
+    }
 
-	resp.Buttons(NewButton(title, url, hide, p))
-	return resp
+    resp.Buttons(NewButton(title, url, hide, p))
+    return resp
 }
 
 // Buttons добавляет одну или несколько кнопок в ответ.
 func (resp *Response) Buttons(buttons ...Button) *Response {
-	resp.Response.Buttons = append(resp.Response.Buttons, buttons...)
-	return resp
+    resp.Response.Buttons = append(resp.Response.Buttons, buttons...)
+    resp.Buttons_ = append(resp.Response.Buttons, buttons...)
+    return resp
 }
 
 // EndSession флаг о закрытии сессии.
 func (resp *Response) EndSession() *Response {
-	resp.Response.EndSession = true
-	return resp
+    resp.Response.EndSession = true
+    resp.EndSession_ = true
+    return resp
 }
 
 // Phrase структура фразы.
 type Phrase struct {
-	Text string
-	TTS  string
+    Text string
+    TTS  string
 }
 
 // NewPhrase создает фразу с текстом и TTS.
 func NewPhrase(text, tts string) Phrase {
-	return Phrase{
-		Text: text,
-		TTS:  tts,
-	}
+    return Phrase{
+	Text: text,
+	TTS:  tts,
+    }
 }
 
 // Phrase добавляет к тексту и TTS ответа данные фразы.
 func (resp *Response) Phrase(p ...Phrase) *Response {
-	ix := rand.Intn(len(p))
-	resp.Response.Text += p[ix].Text
-	resp.Response.TTS += p[ix].TTS
-	return resp
+    ix := rand.Intn(len(p))
+    resp.Response.Text += p[ix].Text
+    resp.Response.TTS += p[ix].TTS
+    resp.Text_ += p[ix].Text
+    resp.TTS_ += p[ix].TTS
+    return resp
 }
 
 // RandomPhrase добавляет к тексту и TTS ответа данные случайной фразы из числа предложенных.
 func (resp *Response) RandomPhrase(p ...Phrase) *Response {
-	ix := rand.Intn(len(p))
-	resp.Response.Text += p[ix].Text
-	resp.Response.TTS += p[ix].TTS
-	return resp
+    ix := rand.Intn(len(p))
+    resp.Response.Text += p[ix].Text
+    resp.Response.TTS += p[ix].TTS
+    resp.Text_ += p[ix].Text
+    resp.TTS_ += p[ix].TTS
+    return resp
 }
 
 // AddSessionState добавляет па
 func (resp *Response) AddSessionState(key string, data interface{}) error {
-	if resp.SessionState == nil {
-		resp.SessionState = make(map[string]interface{})
-	}
+    if resp.SessionState == nil {
+	resp.SessionState = make(map[string]interface{})
+    }
 
-	if stateBag, ok := resp.SessionState.(map[string]interface{}); ok {
-		stateBag[key] = data
-		resp.SessionState = stateBag
-	} else {
-		return errors.New(fmt.Sprintf("session_state: can't set data because session_state have type %T",
-			resp.SessionState))
-	}
-	return nil
+    if stateBag, ok := resp.SessionState.(map[string]interface{}); ok {
+	stateBag[key] = data
+	resp.SessionState = stateBag
+    } else {
+	return errors.New(fmt.Sprintf("session_state: can't set data because session_state have type %T",
+	    resp.SessionState))
+    }
+    return nil
 }
